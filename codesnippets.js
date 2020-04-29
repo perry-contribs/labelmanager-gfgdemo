594be76aadb26c9cfebb3acc02080958364d83c2
Parikshit Hooda <phooda804@live.com>
2020-04-29 12:49:59 +0530

    Create codesnippets.js

---
+++ codesnippets.js
@@ -0,0 +1,19 @@
+      // return;
+      // } else {
+      //   var params = context.issue({
+      //     body:
+      //       "improper command. follow the syntax: /lm [add/remove] label1 label2 label3"
+      //   });
+      //   return context.github.issues.createcomment(params);
+      // }
+      
+      
+              // var labels = labelvals.slice(1, label_len);
+      //         octokit.issues.removeLabel({
+      //   owner,
+      //   repo,
+      //   issue_number,
+      //   name,
+      // });
+      
+            // return context.github.issues.addLabels(context.issue({ labels }));
